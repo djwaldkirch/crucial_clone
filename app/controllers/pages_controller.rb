ded303e91f4fcb6519689c64a4dc225b09a6f86a
Dan Waldkirch <djwaldkirch@gmail.com>
2018-08-16 16:04:42 +0000

    Setting up Heroku

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,11 @@
 class PagesController < ApplicationController
     def home
+        @title = "Crucial Music | Home"
     end
     def about
+        @title = "Crucial Music | About"
+    end
+    def faq
+        @title = "Crucial Music | FAQ"
     end
 end
