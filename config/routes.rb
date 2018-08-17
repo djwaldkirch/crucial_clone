bd8c54bda39df9d4b12c8c4e53efc8816910e888
Dan Waldkirch <djwaldkirch@gmail.com>
2018-08-17 17:54:58 +0000

    Added signup page, trying to get error messages to show

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'pages#home'
-  get 'about', to: "pages#about"
-  get 'faq', to: "pages#faq"
-  
+  get '/about', to: "pages#about"
+  get '/faq', to: "pages#faq"
+  get '/signup', to: "users#new"
+  post '/signup',  to: 'users#create'
   resources :users
 end
